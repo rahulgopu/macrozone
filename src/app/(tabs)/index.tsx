eda3c0be2ce7112a2a7cddb8a855d99dc2ffe589
Rahul Gopu <keerthana.h1509@gmail.com>
2026-04-28 14:09:46 -0500

    add async storage to store data

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,17 +1,33 @@
-import { globalStyles } from "@/styles/global";
-import { Link } from "expo-router";
-import { Text, ScrollView } from "react-native";
-import HomeHeader from "@/components/HomeHeader";
-import MacroGrid from "@/components/MacroGrid";
-import RecentMeals from "@/components/RecentMeals";
+import HomeHeader from '@/components/HomeHeader';
+import MacroGrid from '@/components/MacroGrid';
+import RecentMeals from '@/components/RecentMeals';
+import { getMeals, Meal } from '@/storage/meals';
+import { globalStyles } from '@/styles/global';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { ScrollView, Text } from 'react-native';
 
 export default function HomeScreen() {
-	return (
-		<ScrollView style={globalStyles.container}>
-			<Text style={globalStyles.title}>MacroZone</Text>
-			<HomeHeader />
-			<MacroGrid />
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
+  return (
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>MacroZone</Text>
+      <HomeHeader />
+      <MacroGrid />
       <RecentMeals />
-		</ScrollView>
-	);
+    </ScrollView>
+  );
 }
